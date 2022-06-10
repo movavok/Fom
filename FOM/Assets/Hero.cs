75963a96c6db8c500e66e21eb2379ef78f4485c1
SHER0 <nikitapolovoy1@gmail.com>
2022-06-10 16:17:37 +0200

    стабильная версия

--- FOM/Assets/Hero.cs
+++ FOM/Assets/Hero.cs
@@ -4,56 +4,26 @@ using UnityEngine;
 
 public class Hero : MonoBehaviour
 {
-    [SerializeField] float speed = 7f;
-    [SerializeField] int lives = 5;
-    [SerializeField] float jumpForce = 10f;
-    bool Ground = false;
-
+    public float speed = 7f;
+    public float jumpForce = 5f;
+    
     Rigidbody2D rb;
-    SpriteRenderer sprite;
-
-
+    
+    // Start is called before the first frame update
     void Start()
     {
-
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButton("Horizontal"))
-            Run();
-        if (Ground && Input.GetButton("Jump"))
-            Jump();
-    }
+        float movement = Input.GetAxis("Horizontal");
+        transform.position += new Vector3(movement, 0, 0) * speed * Time.deltaTime;
 
-    void Awake()
-    {
-        rb = GetComponent<Rigidbody2D>();
-        sprite = GetComponentInChildren<SpriteRenderer>();
-    }
 
-    void Run()
-    {
-        Vector3 dir = transform.right * Input.GetAxis("Horizontal");
-        transform.position = Vector3.MoveTowards(transform.position, transform.position + dir, speed * Time.deltaTime);
-
-        sprite.flipX = dir.x < 0.0f;
-    }
-
-    void Jump()
-    {
-        rb.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
-    }
-
-    void Grounded()
-    {
-        Collider2D[] collider = Physics2D.OverlapCircleAll(transform.position, 1f);
-        Ground = collider.Length > 1;
-    }
-
-    void FixedUpdate()
-    {
-        Grounded();
+        if(Input.GetKeyDown(KeyCode.Space))
+        rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+       
     }
 }
