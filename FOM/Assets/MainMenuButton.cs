fd47f1c3ebef2b5a4312f4ea5212faa9dad8d6fe
SHER0 <nikitapolovoy1@gmail.com>
2022-06-11 10:06:49 +0200

    main menu

---
+++ FOM/Assets/MainMenuButton.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MainMenuButton : MonoBehaviour
+{
+    public void StartButton()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+
+    public void ExitButton()
+    {
+        Application.Quit();
+    }
+
+}
