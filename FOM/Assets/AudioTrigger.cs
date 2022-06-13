e0938a360a63346de7f99c33764e1315b6353f47
SHER0 <nikitapolovoy1@gmail.com>
2022-06-13 10:43:55 +0200

    //

--- FOM/Assets/AudioTrigger.cs
+++ FOM/Assets/AudioTrigger.cs
@@ -4,25 +4,10 @@ using UnityEngine;
 
 public class AudioTrigger : MonoBehaviour
 {
-    public AudioSourse musicAudio;
-    private AudioClip clip;
-    
-    private bool isTriggered = false;
-    
-    private void Start()
-    {
-       clip = UWU Sound.clip;
-    }
+    public AudioSource playSound;
 
-    private void OnTriggerEnter(Collider other)
+    void OnTriggerEnter(Collider other)
     {
-        if(other.tag == "Player" || other.tag == "Player1")
-        {
-            if(isTriggered == false)
-            {
-                isTriggered = true;
-                musicAudio.PlayOneShot(clip);
-            }
-        }
+        playSound.Play();
     }
 }
