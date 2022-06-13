4efcc2949dcc08b0970552535e9adf7a4626608a
SHER0 <nikitapolovoy1@gmail.com>
2022-06-13 21:14:14 +0200

    робит текст

---
+++ FOM/Assets/Scripts/TextG.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextG : MonoBehaviour
+{
+    private string text;
+    
+    private void Start()
+    {
+        text = GetComponent<Text>().text;
+        GetComponent<Text>().text = "";
+        StartCoroutine(TextCoroutine());
+        
+    }
+
+    IEnumerator TextCoroutine()
+    {
+        foreach (char abc in text)
+        {
+            GetComponent<Text>().text += abc;
+            yield return new WaitForSeconds(0.1f);
+            
+        }
+    }
+
+  
+}
